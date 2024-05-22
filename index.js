7b7dd04d2e7f0456f923741f77faa3b570be4c07
Labelle Cândido <126924842+LABELLECANDIDO@users.noreply.github.com>
2024-05-22 13:34:20 +0000

    commit 1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express()
+
+const port = 3000
+
+let pessoas = [
+  {
+    nome: 'João',
+    idade: 18,
+    id: 1
+  },
+  {
+    nome: 'João',
+    idade: 18,
+    id: 2
+  }
+]
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  console.log('bati na raiz')
+	res.send('estou na minha api')
+})
+
+app.get('/pessoas', (req, res) => {
+  console.log('bati na pessoa')
+  res.json('estou na pessoa')
+})
+
+app.post('/pessoas', (req, res) =>{
+  console.log('bati na pessoa')
+  const pessoa = req.body
+  pessoas.push(pessoa)
+  res.json(pessoas)
+})
+
+app.listen(()=>{
+  console.log('server is up', port)
+})
